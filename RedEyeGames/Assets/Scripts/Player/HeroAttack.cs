d5c824562b37524d7c2ecff2822dab49b1afb36f
bjc3295 <55869180+bjc3295@users.noreply.github.com>
2020-11-14 16:23:59 -0800

    stuff happened

--- RedEyeGames/Assets/Scripts/Player/HeroAttack.cs
+++ RedEyeGames/Assets/Scripts/Player/HeroAttack.cs
@@ -9,15 +9,24 @@ public class HeroAttack : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private LayerMask enemyLayers;
     [SerializeField] private Transform attackPoint;
+    [SerializeField] private WeaponGlow weaponGlow;
+    [SerializeField] private int powerUp;
     [SerializeField] private float attackRange = 0.4f;
-    [SerializeField] private int attackDamage = 50;
+    [SerializeField] private int baseAttackDamage = 10;
+    private int modifiedAttackDamage;
     [SerializeField] private float attackRate = 2f;
+    [SerializeField] private float attackForce = 1f;
     private float nextAttackTime = 0f;
+    [SerializeField] private int additionalFireDamage = 5;
+    [SerializeField] private int additionalRunDamage = 5;
+    [SerializeField] private int poisonDamage = 1;
+    [SerializeField] private int poisonDuration = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         health = GetComponent<Health>();
+        weaponGlow = GetComponent<WeaponGlow>();
     }
 
     // Update is called once per frame
@@ -30,7 +39,7 @@ public class HeroAttack : MonoBehaviour
         {
             if (Input.GetButtonDown("Fire1"))
             {
-                Attack();
+                animator.SetTrigger("Attack"); // attack function is within the animation
                 nextAttackTime = Time.time + 1f / attackRate;
             }
         }
@@ -38,13 +47,22 @@ public class HeroAttack : MonoBehaviour
 
     void Attack()
     {
-        animator.SetTrigger("Attack");
+        powerUp = weaponGlow.getPowerUp();
+
+        if (powerUp == 4)
+            modifiedAttackDamage = baseAttackDamage + additionalFireDamage;
+        else
+            modifiedAttackDamage = baseAttackDamage;
+
+        if (animator.GetFloat("Speed") > 16)
+            modifiedAttackDamage = modifiedAttackDamage + additionalRunDamage;
 
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         foreach (Collider2D enemy in hitEnemies)
         {
-            if (!enemy.GetComponent<EnemyController>().IsDead())
-                enemy.GetComponent<EnemyController>().Hurt(attackDamage);
+            enemy.GetComponent<EnemyController>().Hurt(modifiedAttackDamage, attackForce, this.transform);
+            if (powerUp == 3)
+                enemy.GetComponent<EnemyController>().DamageOverTime(poisonDamage, poisonDuration);
         }
     }
 
