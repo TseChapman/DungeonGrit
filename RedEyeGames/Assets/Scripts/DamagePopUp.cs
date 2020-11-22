d9dd1fa105899a791ed52276e5c5dc2658bd6b42
bjc3295 <55869180+bjc3295@users.noreply.github.com>
2020-11-22 13:00:25 -0800

    menus mostly

---
+++ RedEyeGames/Assets/Scripts/DamagePopUp.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamagePopUp : MonoBehaviour
+{
+    private static int sortingOrder;
+
+    private const float DISAPPEAR_TIMER_MAX = 1f;
+
+    private TextMeshPro textMesh;
+    private float disappearTimer;
+    private Color textColor;
+
+    public static DamagePopUp CreatePlayer(Vector3 position, int damage)
+    {
+        Transform damagePopUpTransform = Instantiate(GameAssets.i.pfDamagePopUpPlayer, position, Quaternion.identity);
+        DamagePopUp damagePopUp = damagePopUpTransform.GetComponent<DamagePopUp>();
+        damagePopUp.Setup(damage);
+
+        return damagePopUp;
+    }
+
+    public static DamagePopUp CreateEnemy(Vector3 position, int damage)
+    {
+        Transform damagePopUpTransform = Instantiate(GameAssets.i.pfDamagePopUpEnemy, position, Quaternion.identity);
+        DamagePopUp damagePopUp = damagePopUpTransform.GetComponent<DamagePopUp>();
+        damagePopUp.Setup(damage);
+
+        return damagePopUp;
+    }
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshPro>();
+    }
+
+    public void Setup(int damage)
+    {
+        textMesh.SetText(damage.ToString());
+        textColor = textMesh.color;
+        disappearTimer = DISAPPEAR_TIMER_MAX;
+
+        sortingOrder++;
+        textMesh.sortingOrder = sortingOrder;
+    }
+
+    private void Update()
+    {
+        float moveYSpeed = 0.75f;
+        transform.position += new Vector3(0, moveYSpeed * Time.smoothDeltaTime);
+
+        if (disappearTimer > DISAPPEAR_TIMER_MAX * 0.5f)
+        {
+            // first half of popup lifetime
+            float increaseScaleAmount = 1f;
+            transform.localScale += Vector3.one * increaseScaleAmount * Time.smoothDeltaTime;
+        }
+        else
+        {
+            // second half of popup lifetime
+            float decreaseScaleAmount = 1f;
+            transform.localScale -= Vector3.one * decreaseScaleAmount * Time.smoothDeltaTime;
+        }
+
+        disappearTimer -= Time.smoothDeltaTime;
+        if (disappearTimer < 0)
+        {
+            // start disappearing
+            float disappearSpeed = 3f;
+            textColor.a -= disappearSpeed * Time.smoothDeltaTime;
+            textMesh.color = textColor;
+            if (textColor.a < 0)
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ RedEyeGames/Assets/Scripts/Traps/DartLauncher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class DartLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject dart;
+    [SerializeField] private float rateOfFire = 1f;
+    private float lastShot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (lastShot + rateOfFire < Time.time)
+            {
+                if (GetComponent<SpriteRenderer>().flipX)
+                    Instantiate(dart, transform.position, Quaternion.AngleAxis(180, Vector3.up));
+                else
+                    Instantiate(dart, transform.position, Quaternion.identity);
+                lastShot = Time.time;
+            }
+        }
+    }
+}
